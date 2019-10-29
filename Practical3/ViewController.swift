4d020d1f414c514f93d8a78b91f5650119d59888
maxneo <s10189580@connect.np.edu.sg>
2019-10-29 16:25:09 +0800

    Initial Commit

---
+++ Practical3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practical3
+//
+//  Created by MAD2_P01 on 29/10/19.
+//  Copyright © 2019 NeoGuoBin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
